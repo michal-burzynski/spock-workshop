79dda2430353c7e25cea25e49b5df2fa8b31ce51
Michal Burzynski <mburzyn@gmail.com>
2016-05-18 22:28:53 +0200

    remove answers

--- src/test/groovy/com/capgemini/spock/service/HelloServiceSpec.groovy
+++ src/test/groovy/com/capgemini/spock/service/HelloServiceSpec.groovy
@@ -8,17 +8,10 @@ class HelloServiceSpec extends Specification{
 	
 	def "should send message once to all recipients"() {
 		given:
-		def message = "Hello"
-		def firstRecipient = Mock(MessageRecipient)
-		MessageRecipient secondRecipient = Mock()
-		def helloService = new HelloService([firstRecipient, secondRecipient])
 		
 		when:
-		def result = helloService.broadcastMessage(message)
 		
 		then:
-		1 * firstRecipient.receiveMessage(message) >> "ERROR"
-		1 * secondRecipient.receiveMessage(message) >> "OK"
-		result == ["ERROR", "OK"]
+		
 	}
 }
